0fa22ac54f37537a11de894d63dcd8c283031539
carlosbur <carlosbur@gmail.com>
2023-07-26 11:27:25 -0300

    trabajamos sobre el checkout

--- src/screens/cart/styles.js
+++ src/screens/cart/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { COLORS } from "../../themes";
 
 export const styles = StyleSheet.create({
     container: {
@@ -6,5 +7,44 @@ export const styles = StyleSheet.create({
     },
     listContainer:{
         flex: 1,
+        backgroundColor: COLORS.white,
     },
+    footerContainer: {
+        backgroundColor: COLORS.background,
+        borderTopColor: COLORS.secondary,
+        borderTopWidth: 4,
+        padding: 10,
+    },
+
+checkoutButton: {
+    backgroundColor: COLORS.secondary,
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
+},
+
+checkoutButtonText: {
+    color: COLORS.white,
+    fontFamily: 'Montserrat-Bold',
+},
+
+totalContainer: {
+    flexDirection: 'row',
+    gap: 5,
+},
+
+totalText: {
+    color: COLORS.white,
+    fontFamily: 'Montserrat-Bold',
+},
+
+totalPrice: {
+    color: COLORS.white,
+    fontFamily: 'Montserrat-Bold',
+},
+
 });
