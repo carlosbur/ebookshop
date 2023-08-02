16bfd99d23552bf4077620e1cd289858eb453358
carlosbur <carlosbur@gmail.com>
2023-08-02 09:37:09 -0300

    estilizamos pantalla de login y registro

--- src/screens/auth/index.jsx
+++ src/screens/auth/index.jsx
@@ -1,11 +1,50 @@
+import { useState } from "react";
 import { styles } from "./styles";
-
-import { View, Text } from "react-native";
+import { COLORS } from "../../themes";
+import { View, Text, TouchableOpacity } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 const Auth = () => {
+    const [isLogin, setIsLogin] = useState(true);
+    const headerTitle =  isLogin ? 'Login' : 'Register';
+    const buttonTitle =  isLogin ? 'Login' : 'Register';
+    const messageText =  isLogin ? 'Need an acount?' : 'Already have an acount';
+
     return (
         <View style={styles.container}>
-            <Text>Auth</Text>
+            <View style={styles.content}>
+                <Text style={styles.header}>{headerTitle}</Text>
+                <Text style={styles.label}>Email</Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="email@domain.com"
+                    placeholderTextColor={COLORS.grey}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    onChangeText={()=> {}}
+                />
+                <Text style={styles.label}>Password</Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="*************"
+                    placeholderTextColor={COLORS.grey}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry={true}
+                    onChangeText={()=> {}}
+                />
+            
+                <View style={styles.linkContainer}>
+                    <TouchableOpacity style={styles.link} onPress={()=> setIsLogin(!isLogin)}>
+                        <Text style={styles.linkText}>{messageText}</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text styles={styles.buttonText}> {buttonTitle} </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
--- src/screens/auth/styles.js
+++ src/screens/auth/styles.js
@@ -1,10 +1,89 @@
 import { StyleSheet } from "react-native";
+import { COLORS } from "../../themes";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: COLORS.background
     },
+
+    content: {
+        width: '80%',
+        maxWidth: 400,
+        padding: 15,
+        margin: 15,
+        backgroundColor: COLORS.white,
+        borderWidth: 1,
+        borderColor: COLORS.primary,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+
+        elevation: 3,
+        borderRadius: 5,
+    },
+    header: {
+        fontFamily: 'Montserrat-Medium',
+        fontSize: 16,
+        textAlign: 'center',
+        color: COLORS.text,
+        paddingVertical: 10,
+    },
+    label: {
+        fontFamily: 'Montserrat-Light',
+        fontSize: 14,
+        color: COLORS.text,
+    },
+    input: {
+        height: 45,
+        borderBottomColor: COLORS.primary,
+        borderBottomWidth: 1,
+        width: '90%',
+        fontFamily: 'Montserrat-Regular',
+        marginBottom: 5,
+    },
+    linkContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    link: {
+
+    },
+    linkText: {
+        fontFamily: 'Montserrat-Medium',
+        fontSize: 14,
+        textAlign: 'center',
+        color: COLORS.primary,
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 10,
+
+    },
+    button: {
+        backgroundColor: COLORS.secondary,
+        width: 200,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+
+
+    },
+    buttonText: {
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 14,
+        textAlign: 'center',
+        color: COLORS.white,
+
+    }
 
 })
